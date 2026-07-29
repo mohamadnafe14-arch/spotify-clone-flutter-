0816d9d067bc12416cc10980110825b56b3998c8
Mohamed <mohamadnafe14@gmail.com>
2026-07-29 22:31:14 +0300

    adding add song view

---
+++ lib/features/home/views/add_song_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddSongView extends StatefulWidget {
+  const AddSongView({super.key});
+
+  @override
+  State<AddSongView> createState() => _AddSongViewState();
+}
+
+class _AddSongViewState extends State<AddSongView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
